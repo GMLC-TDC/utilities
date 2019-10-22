622eca8871dca554241400be9d5c60dbf9d9d1aa
Philip Top <top1@llnl.gov>
2019-10-22 14:55:39 -0700

    update googletest

--- config/cmake/AddGoogletest.cmake
+++ config/cmake/AddGoogletest.cmake
@@ -8,50 +8,56 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #
-# Downloads GTest and provides a helper macro to add tests. Add make check, as well, which
-# gives output on failed tests without having to set an environment variable.
+# Downloads GTest and provides a helper macro to add tests. Add make check, as well,
+# which gives output on failed tests without having to set an environment variable.
 #
-include(extraMacros)
+
 set(gtest_version release-1.10.0)
-#depending on what the version is set to the git_clone command may need to change to GIT_TAG||GIT_BRANCH|GIT_COMMIT
+set(gtest_vtype GIT_TAG)
+# depending on what the version is set to the git_clone command may need to change to
+# GIT_TAG||GIT_BRANCH|GIT_COMMIT
 
 string(TOLOWER "googletest" gtName)
 
 if(NOT CMAKE_VERSION VERSION_LESS 3.11)
-include(FetchContent)
-mark_as_advanced(FETCHCONTENT_BASE_DIR)
-mark_as_advanced(FETCHCONTENT_FULLY_DISCONNECTED)
-mark_as_advanced(FETCHCONTENT_QUIET)
+    include(FetchContent)
+    mark_as_advanced(FETCHCONTENT_BASE_DIR)
+    mark_as_advanced(FETCHCONTENT_FULLY_DISCONNECTED)
+    mark_as_advanced(FETCHCONTENT_QUIET)
 
-FetchContent_Declare(
-  googletest
-  GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG        ${gtest_version}
-)
+    fetchcontent_declare(
+        googletest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG ${gtest_version}
+    )
 
-FetchContent_GetProperties(googletest)
+    fetchcontent_getproperties(googletest)
 
-if(NOT ${gtName}_POPULATED)
-  # Fetch the content using previously declared details
-  FetchContent_Populate(googletest)
+    if(NOT ${gtName}_POPULATED)
+        # Fetch the content using previously declared details
+        fetchcontent_populate(googletest)
 
-endif()
-hide_variable(FETCHCONTENT_SOURCE_DIR_GOOGLETEST)
-hide_variable(FETCHCONTENT_UPDATES_DISCONNECTED_GOOGLETEST)
-else() #cmake <3.11
+    endif()
+    hide_variable(FETCHCONTENT_SOURCE_DIR_GOOGLETEST)
+    hide_variable(FETCHCONTENT_UPDATES_DISCONNECTED_GOOGLETEST)
+else() # cmake <3.11
 
-# create the directory first
-file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/_deps)
+    # create the directory first
+    file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/_deps)
 
-include(GitUtils)
-git_clone(
-             PROJECT_NAME                    googletest
-             GIT_URL                         https://github.com/google/googletest.git
-             GIT_TAG                         ${gtest_version}
-			 DIRECTORY                       ${PROJECT_BINARY_DIR}/_deps
-       )
-	   
-set(${gtName}_BINARY_DIR ${PROJECT_BINARY_DIR}/_deps/${gtName}-build)
+    include(GitUtils)
+    git_clone(
+        PROJECT_NAME
+        googletest
+        GIT_URL
+        https://github.com/google/googletest.git
+        ${gtest_vtype}
+        ${gtest_version}
+        DIRECTORY
+        ${PROJECT_BINARY_DIR}/_deps
+    )
+
+    set(${gtName}_BINARY_DIR ${PROJECT_BINARY_DIR}/_deps/${gtName}-build)
 
 endif()
 
@@ -64,11 +70,10 @@ set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS 1 CACHE INTERNAL "")
 add_subdirectory(${${gtName}_SOURCE_DIR} ${${gtName}_BINARY_DIR} EXCLUDE_FROM_ALL)
 
 message(STATUS "loading google-test directory ${${gtName}_SOURCE_DIR}")
-if (NOT MSVC)
-#target_Compile_options(gtest PRIVATE "-Wno-undef")
-#target_Compile_options(gmock PRIVATE "-Wno-undef")
-#target_Compile_options(gtest_main PRIVATE "-Wno-undef")
-#target_Compile_options(gmock_main PRIVATE "-Wno-undef")
+if(NOT MSVC)
+    # target_Compile_options(gtest PRIVATE "-Wno-undef") target_Compile_options(gmock
+    # PRIVATE "-Wno-undef") target_Compile_options(gtest_main PRIVATE "-Wno-undef")
+    # target_Compile_options(gmock_main PRIVATE "-Wno-undef")
 endif()
 
 if(GOOGLE_TEST_INDIVIDUAL)
@@ -79,29 +84,31 @@ if(GOOGLE_TEST_INDIVIDUAL)
     endif()
 endif()
 
-function(add_unit_test test_source_file)
-  get_filename_component(test_name "${test_source_file}" NAME_WE)
-  add_executable("${test_name}" "${test_source_file}")
-  target_link_libraries("${test_name}" gtest gmock gtest_main)
-  add_test(NAME ${test_name} COMMAND $<TARGET_FILE:${test_name}>)
-   set_target_properties(${test_name} PROPERTIES FOLDER "Tests")
-endfunction()
-
 # Target must already exist
 macro(add_gtest TESTNAME)
     target_link_libraries(${TESTNAME} PUBLIC gtest gmock gtest_main)
-    
+
     if(GOOGLE_TEST_INDIVIDUAL)
         if(CMAKE_VERSION VERSION_LESS 3.10)
-            gtest_add_tests(TARGET ${TESTNAME}
-                            TEST_PREFIX "${TESTNAME}."
-                            TEST_LIST TmpTestList)
+            gtest_add_tests(
+                TARGET
+                ${TESTNAME}
+                TEST_PREFIX
+                "${TESTNAME}."
+                TEST_LIST
+                TmpTestList
+            )
             set_tests_properties(${TmpTestList} PROPERTIES FOLDER "Tests")
         else()
-            gtest_discover_tests(${TESTNAME}
-                TEST_PREFIX "${TESTNAME}."
-                PROPERTIES FOLDER "Tests")
-            
+            gtest_discover_tests(
+                ${TESTNAME}
+                TEST_PREFIX
+                "${TESTNAME}."
+                PROPERTIES
+                FOLDER
+                "Tests"
+            )
+
         endif()
     else()
         add_test(${TESTNAME} ${TESTNAME})
@@ -119,16 +126,18 @@ hide_variable(BUILD_GMOCK)
 hide_variable(BUILD_GTEST)
 hide_variable(INSTALL_GTEST)
 
-
-set_target_properties(gtest gtest_main gmock gmock_main
-    PROPERTIES FOLDER "Extern")
+set_target_properties(gtest gtest_main gmock gmock_main PROPERTIES FOLDER "Extern")
 
 if(MSVC)
-     #  add_compile_options( /wd4459)
-    if (MSVC_VERSION GREATER_EQUAL 1900)
-        target_compile_definitions(gtest PUBLIC _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING)
-        target_compile_definitions(gtest_main PUBLIC _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING)
-        target_compile_definitions(gmock PUBLIC _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING)
-        target_compile_definitions(gmock_main PUBLIC _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING)
+    # add_compile_options( /wd4459)
+    if(MSVC_VERSION GREATER_EQUAL 1900)
+        target_compile_definitions(gtest PUBLIC
+                                   _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING)
+        target_compile_definitions(gtest_main PUBLIC
+                                   _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING)
+        target_compile_definitions(gmock PUBLIC
+                                   _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING)
+        target_compile_definitions(gmock_main PUBLIC
+                                   _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING)
     endif()
 endif()
--- config/cmake/GitUtils.cmake
+++ config/cmake/GitUtils.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 2.8.7)
 
-include(Utils)
+include(${PROJECT_SOURCE_DIR}/config/cmake/Utils.cmake)
 include(CMakeParseArguments)
 
 find_package(Git)
