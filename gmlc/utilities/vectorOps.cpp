9f4be83924c4d7e875ef643db1df53ac7552299e
Philip Top <phlptp@gmail.com>
2022-07-07 18:30:28 -0700

    update vectorOps for clang tidy

--- gmlc/utilities/vectorOps.cpp
+++ gmlc/utilities/vectorOps.cpp
@@ -12,21 +12,20 @@
 
 #include "vectorOps.hpp"
 
-namespace gmlc {
-namespace utilities {
+namespace gmlc::utilities {
     double solve2x2(
         double v11,
         double v12,
         double v21,
         double v22,
-        double y1,
-        double y2,
-        double& x1,
-        double& x2)
+        double input1,
+        double input2,
+        double& output1,
+        double& output2)
     {
         double det = 1.0 / (v11 * v22 - v12 * v21);
-        x1 = det * (v22 * y1 - v12 * y2);
-        x2 = det * (-v21 * y1 + v11 * y2);
+        output1 = det * (v22 * input1 - v12 * input2);
+        output2 = det * (-v21 * input1 + v11 * input2);
         return det;
     }
 
@@ -58,12 +57,12 @@ namespace utilities {
 
         double deti = 1.0 / (a11 * i11 + a21 * i12 + a31 * i13);
 
-        std::array<double, 3> Y{
+        std::array<double, 3> output{
             {deti * (i11 * vals[0] + i12 * vals[1] + i13 * vals[2]),
              deti * (i21 * vals[0] + i22 * vals[1] + i23 * vals[2]),
              deti * (i31 * vals[0] + i32 * vals[1] + i33 * vals[2])}};
 
-        return Y;
+        return output;
     }
 
     // Linear Interpolation function
@@ -72,39 +71,38 @@ namespace utilities {
         const std::vector<double>& valIn,
         const std::vector<double>& timeOut)
     {
-        size_t mx = (std::min)(valIn.size(), timeIn.size());
+        size_t minSize = (std::min)(valIn.size(), timeIn.size());
         std::vector<double> out(timeOut.size(), 0);
-        size_t jj = 0;
-        size_t kk = 0;
-        while (timeOut[jj] <= timeIn[0]) {
-            out[jj] = valIn[0] -
+        size_t index1 = 0;
+        size_t index2 = 0;
+        while (timeOut[index1] <= timeIn[0]) {
+            out[index1] = valIn[0] -
                 (valIn[1] - valIn[0]) / (timeIn[1] - timeIn[0]) *
-                    (timeIn[0] - timeOut[jj]);
-            ++jj;
+                    (timeIn[0] - timeOut[index1]);
+            ++index1;
         }
-        while (jj < timeOut.size()) {
-            while (timeIn[kk + 1] < timeOut[jj]) {
-                ++kk;
-                if (kk + 1 == mx) {
+        while (index1 < timeOut.size()) {
+            while (timeIn[index2 + 1] < timeOut[index1]) {
+                ++index2;
+                if (index2 + 1 == minSize) {
                     goto breakLoop;  // break out of a double loop
                 }
             }
-            out[jj] = valIn[kk] +
-                (valIn[kk + 1] - valIn[kk]) / (timeIn[kk + 1] - timeIn[kk]) *
-                    (timeOut[jj] - timeIn[kk]);
+            out[index1] = valIn[index2] +
+                (valIn[index2 + 1] - valIn[index2]) / (timeIn[index2 + 1] - timeIn[index2]) *
+                    (timeOut[index1] - timeIn[index2]);
             // out[jj] = std::fma((valIn[kk + 1] - valIn[kk]) / (timeIn[kk + 1]
             // - timeIn[kk]), (timeOut[jj] - timeIn[kk]), valIn[kk]);
-            ++jj;
+            ++index1;
         }
     breakLoop:
-        while (jj < timeOut.size()) {
-            out[jj] = valIn[mx - 1] +
-                (valIn[mx - 1] - valIn[mx - 2]) /
-                    (timeIn[mx - 1] - timeIn[mx - 2]) *
-                    (timeOut[jj] - timeIn[mx - 1]);
-            ++jj;
+        while (index1 < timeOut.size()) {
+            out[index1] = valIn[minSize - 1] +
+                (valIn[minSize - 1] - valIn[minSize - 2]) /
+                    (timeIn[minSize - 1] - timeIn[minSize - 2]) *
+                    (timeOut[index1] - timeIn[minSize - 1]);
+            ++index1;
         }
         return out;
     }
-}  // namespace utilities
 }  // namespace gmlc
