ab05b4810d09dc9f45987167935c7450a34c9c83
Philip Top <phlptp@gmail.com>
2022-09-30 16:40:44 -0700

    update MapOps

--- gmlc/utilities/mapOps.hpp
+++ gmlc/utilities/mapOps.hpp
@@ -1,18 +1,13 @@
 /*
- * LLNS Copyright Start
- * Copyright (c) 2014-2018, Lawrence Livermore National Security
- * This work was performed under the auspices of the U.S. Department
- * of Energy by Lawrence Livermore National Laboratory in part under
- * Contract W-7405-Eng-48 and in part under Contract DE-AC52-07NA27344.
- * Produced at the Lawrence Livermore National Laboratory.
- * All rights reserved.
- * For details, see the LICENSE file.
- * LLNS Copyright End
- */
+Copyright (c) 2017-2022,
+Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance
+for Sustainable Energy, LLC.  See the top-level NOTICE for additional details.
+All rights reserved. SPDX-License-Identifier: BSD-3-Clause
+*/
 
 #pragma once
 
-#include "optionalDef.hpp"
+#include <optional>
 
 #include <map>
 #include <unordered_map>
@@ -25,7 +20,7 @@ inline X2 mapFind(const std::map<X1, X2>& mapS, const X1& val, const X2& defVal)
 }
 
 template<class X1, class X2>
-utilities::optional<X2> mapFind(const std::map<X1, X2>& mapS, const X1& val)
+std::optional<X2> mapFind(const std::map<X1, X2>& mapS, const X1& val)
 {
     auto map_it = mapS.find(val);
     if (map_it != mapS.end()) {
@@ -45,7 +40,7 @@ inline X2 mapFind(
 }
 
 template<class X1, class X2>
-utilities::optional<X2>
+std::optional<X2>
     mapFind(const std::unordered_map<X1, X2>& mapS, const X1& val)
 {
     auto map_it = mapS.find(val);
