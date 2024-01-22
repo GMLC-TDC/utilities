8895992b69d5e4c79a86ef9a81109286728076b7
Philip Top <phlptp@gmail.com>
2024-01-22 07:17:18 -0800

    add missing header to files

--- gmlc/utilities/stringConversion.h
+++ gmlc/utilities/stringConversion.h
@@ -25,6 +25,7 @@ All rights reserved. SPDX-License-Identifier: BSD-3-Clause
 #include <string>
 #include <type_traits>
 #include <vector>
+#include <cstdint>
 
 namespace gmlc {
 namespace utilities {
