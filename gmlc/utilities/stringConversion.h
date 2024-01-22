0a82fa18d9cecbecaa96d1b29f09bf3c75ee27aa
Philip Top <phlptp@gmail.com>
2024-01-22 07:30:24 -0800

    add missing header to files (#22)
    * add missing header to files
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- gmlc/utilities/stringConversion.h
+++ gmlc/utilities/stringConversion.h
@@ -22,6 +22,7 @@ All rights reserved. SPDX-License-Identifier: BSD-3-Clause
 #include "charMapper.h"
 #include "stringOps.h"
 
+#include <cstdint>
 #include <string>
 #include <type_traits>
 #include <vector>
